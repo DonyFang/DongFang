71d6016475bf7e466b0ff81007dd42b8c0afb455
fdd <978805355@qq.com>
2016-06-22 14:34:25 +0800

    初始化项目

---
+++ GitText/GitText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitText
+//
+//  Created by 方冬冬 on 16/6/22.
+//  Copyright © 2016年 CLTCamelot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitText/GitText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitText
+//
+//  Created by 方冬冬 on 16/6/22.
+//  Copyright © 2016年 CLTCamelot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
